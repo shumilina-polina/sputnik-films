09826d1043e146f8102cdd08c3785ac29bf98b9b
shumilina-polina <shumilina246@gmail.com>
2023-06-20 20:06:37 +0300

    start: about page

---
+++ src/constants/nominations.js
@@ -0,0 +1,20 @@
+export const nominations = [
+  {
+    slug: "moviestart.ru",
+    url: "https://moviestart.ru",
+    text: "Уличные экстремальные виды спорта будут представлены в фильме, поддержанном ФПРК",
+    imageSrc: "nomination-img-1.jpg",
+  },
+  {
+    slug: "moviestart.ru",
+    url: "https://moviestart.ru",
+    text: "Док-победитель Сибирского питчинга дебютантов выходит в российский прокат",
+    imageSrc: "nomination-img-1.jpg",
+  },
+  {
+    slug: "moviestart.ru",
+    url: "https://moviestart.ru",
+    text: "Для нас питчинг стал поворотным событием в жизни",
+    imageSrc: "nomination-img-1.jpg",
+  },
+];
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,8 +1,123 @@
 import Wrapper from "components/Wrapper";
 import s from "./about.module.scss";
+import comanda from "assets/comanda.svg";
+import SocialLink from "components/SocialLink";
+import { styled } from "styled-components";
+import { breakpoints } from "styles/variables";
+import { nominations } from "constants/nominations";
 
 const About = () => {
-  return <Wrapper>About</Wrapper>;
+  return (
+    <section className={s.wr}>
+      <Wrapper>
+        <header>
+          <h1>
+            <img src={comanda} alt="творчество" />
+          </h1>
+          <p>
+            Победители и&nbsp;участники международных кинофестивалей, Фильмы
+            представлены на&nbsp;крупнейших онлайн-платформах
+          </p>
+          <div className={s.links}>
+            <ul>
+              <SocialLink svg={"yandex"}>https://</SocialLink>
+              <SocialLink svg={"yandex"}>https://</SocialLink>
+              <SocialLink svg={"yandex"}>https://</SocialLink>
+              <SocialLink svg={"yandex"}>https://</SocialLink>
+            </ul>
+          </div>
+          <div className={s.authors}>
+            <ul>
+              <Author src={"author-1.png"} name={"Дмитрий Квашнин"}>
+                Основатель агентства, режиссёр, сценарист, продюсер
+              </Author>
+              <Author src={"author-2.png"} name={"Сергей Клейн"}>
+                Сооснователь и&nbsp;исполнительный продюсер
+              </Author>
+            </ul>
+          </div>
+        </header>
+        <main>
+          <h2>публикации и награды:</h2>
+          <ul className={s.nagrady}>
+            {[1, 2, 3, 4, 5].map((elem) => (
+              <li key={elem}>
+                <img
+                  src={require(`assets/nomination-${elem}.png`)}
+                  alt="Номинация"
+                />
+              </li>
+            ))}
+          </ul>
+          <ul className={s.nominacyy}>
+            {nominations.map((elem, i) => (
+              <li key={i}>
+                <a href={elem.url} target="_blank" rel="noopener noreferrer">
+                  {elem.slug}
+                </a>
+                <p>{elem.text}</p>
+                <ImageBox>
+                  <div>
+                    <img
+                      src={require(`assets/${elem.imageSrc}`)}
+                      alt="Номинация"
+                    />
+                  </div>
+                  <div>
+                    <img
+                      src={require(`assets/${elem.imageSrc}`)}
+                      alt="Номинация"
+                    />
+                  </div>
+                </ImageBox>
+              </li>
+            ))}
+          </ul>
+        </main>
+      </Wrapper>
+    </section>
+  );
 };
 
 export default About;
+
+const Author = ({ src, name, children }) => {
+  return (
+    <figure>
+      <ImageBox>
+        <div>
+          <img src={require(`assets/${src}`)} alt={name} />
+        </div>
+        <div>
+          <img src={require(`assets/${src}`)} alt={name} />
+        </div>
+      </ImageBox>
+      <figcaption>
+        <span>{name}</span>
+        <span>{children}</span>
+      </figcaption>
+    </figure>
+  );
+};
+
+const ImageBox = styled.div`
+    width: 124px;
+    aspect-ratio: 1/1;
+    position: relative;
+    @media ${breakpoints.laptop} {
+      width: 6vw;
+    }
+    & > div {
+      position: relative;
+      &:first-child {
+        position: absolute;
+        filter: blur(38px);
+        left: 0;
+        top: 0;
+        bottom: 0;
+        right: 0;
+      }
+    }
+  }
+  
+`;
